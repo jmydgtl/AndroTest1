9e6b4f22d394133b8c4c7a7989835920bc9bc5a6
macmini-xubuntuvm <jmydgtl@gmail.com>
2011-11-27 01:33:30 -0600

    original commit

---
+++ Test Android Project/src/com/james/test/TestAndroidProjectActivity.java
@@ -0,0 +1,13 @@
+package com.james.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestAndroidProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
